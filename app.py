e53a8ddb76405c76120492bbcdd28202b9a2d36a
Chris Moyle <chrismoyle@hotmail.com>
2022-10-25 20:50:49 +0100

    Added browser tab title

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import dash_bootstrap_components as dbc
 #ext_stylesheets = [dbc.themes.LUX]
 external_stylesheets = [dbc.themes.LUX]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, title="Tools for Traders")
 
 server = app.server
 app.config.suppress_callback_exceptions = True
